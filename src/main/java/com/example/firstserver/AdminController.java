e2eea3adbe2ea84b7eeaf327baf904f0332a73e2
cthacker-udel <cthacker@udel.edu>
2021-07-28 03:21:07 -0400

    Added Admin method to list All admin in database

--- src/main/java/com/example/firstserver/AdminController.java
+++ src/main/java/com/example/firstserver/AdminController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 @RestController
 public class AdminController {
@@ -36,6 +40,30 @@ public class AdminController {
         else{
             System.out.println("\n--------- STATUS 400 ---------\nADMIN HAS NOT BEEN CREATED!\n");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @GetMapping("/admin/all")
+    public Object listAllAdmin(){
+
+        try{
+
+            List<Admin> admins = mongoTemplate.findAll(Admin.class);
+            for(Admin eachAdmin : admins){
+
+                System.out.println(eachAdmin.toString());
+
+            }
+            System.out.println("\n--------- STATUS 200 ---------\nADMINS HAVE BEEN LISTED!\n");
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+
+        }
+        catch(Exception e){
+
+            System.out.println("\n--------- STATUS 400 ---------\nREQUEST ERROR!\n");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
         }
 
     }
