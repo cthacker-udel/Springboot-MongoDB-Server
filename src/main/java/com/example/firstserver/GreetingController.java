9fbedd7449016ecacd164f0c967c6355b6a79be3
cthacker-udel <cthacker@udel.edu>
2021-08-02 03:17:08 -0400

    Added custom exception handler

--- src/main/java/com/example/firstserver/GreetingController.java
+++ src/main/java/com/example/firstserver/GreetingController.java
@@ -1,8 +1,11 @@
 package com.example.firstserver;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
